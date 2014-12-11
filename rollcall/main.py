63ec53a651c7f94b5e169dccf88caca3819ae052
Sumit Sahrawat <sumitsahrawat@outlook.com>
2014-12-11 07:37:39 +0530

    Implemented generation of json

--- rollcall/main.py
+++ rollcall/main.py
@@ -2,6 +2,19 @@
 Main module
 """
 
+import json
+from datetime import date, timedelta
+import os
+
+days = ["mon", "tue", "wed", "thu", "fri", "sat"]
+tags = ["attended", "missed", "holiday", "future"]
+
+def dict_to_json(json_dict):
+    return json.dumps(json_dict, indent=4, separators=(',', ': '))
+
+def json_to_dict(json_str):
+    return json.loads(json_str)
+
 def pDir():
     """
     gives the present working directory
@@ -58,3 +71,19 @@ def delete():
     Delete a subject
     """
     pass
+
+def format_date(d, form="%d/%m/%y"):
+    return d.strftime(form)
+
+def gen_dict(semester_start, class_weekdays, semester_weeks=16):
+    class_dates = []
+    for x in xrange(semester_weeks * 7):
+        current_date = semester_start + timedelta(days=x)
+        if current_date.weekday() in class_weekdays:
+            class_dates.append(current_date)
+
+    json_dict = {}
+    for d in class_dates:
+        json_dict[format_date(d)] = "future"
+
+    return json_dict
